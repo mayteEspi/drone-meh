7f9aa5b995658f1011dff851862071f11b7194d6
mayteEspi <maytehermida@gmail.com>
2018-02-08 17:57:06 +0100

    convert to spring boot project

---
+++ api-dron/src/main/java/com/api/drone/ApiDronApplication.java
@@ -0,0 +1,12 @@
+package com.api.drone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDronApplication.class, args);
+	}
+}
