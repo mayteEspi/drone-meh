ee78139a6ae2cf889357f6bd9596ec24fa892916
mayteEspi <maytehermida@gmail.com>
2018-02-19 15:35:36 +0100

    app

--- api-dron/src/test/java/com/api/drone/service/ResidentialServiceTest.java
+++ api-dron/src/test/java/com/api/drone/service/ResidentialServiceTest.java
@@ -1,7 +1,6 @@
 package com.api.drone.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
